201cd716c5e468f6e7e3b0cf6b974bc90d9fe24f
Hussain <muhammadhussain824@gmail.com>
2025-03-11 23:42:57 +0500

    Deployed a Flask-based web application with a PostgreSQL database on a Mini-kube managed kubernetes cluster

---
+++ app/app.py
@@ -0,0 +1,27 @@
+from flask import Flask
+import os
+import psycopg2
+
+app = Flask(__name__)
+
+def get_db_connection():
+    conn = psycopg2.connect(
+        host=os.getenv('DB_HOST'),
+        database=os.getenv('DB_NAME'),
+        user=os.getenv('DB_USER'),
+        password=os.getenv('DB_PASSWORD')
+    )
+    return conn
+
+@app.route('/')
+def index():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('SELECT version();')
+    db_version = cur.fetchone()
+    cur.close()
+    conn.close()
+    return f'Database version: {db_version}'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
